67078c28dd43ebe1726a10bba3376dc7d7182638
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2024-02-09 09:55:33 +0530

    added alias for fields

---
+++ aliases.py
@@ -0,0 +1,23 @@
+ALIASES = {
+	"FN": "Name",
+	"X-ANDROID-CUSTOM":	{
+		"relation" : {
+			'0': "Custom",
+			'1': "Assistant",
+			'2': "Brother",
+			'3': "Child",
+			'4': "Domestic Partner",
+			'5': "Father",
+			'6': "Friend",
+			'7': "Manager",
+			'8': "Mother",
+			'9': "Parent",
+			'10': "Partner",
+			'11': "Referrred",
+			'12': "Relative",
+			'13': "Sister",
+			'14': "Spouse",
+		},
+		"nickname": "Nickname"
+	}
+}
